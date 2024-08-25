b2fa4c073bec3d58e3e5f6ac04a8bb1f23d19da3
saphal4859 <saisaphal123@gmail.com>
2024-08-25 19:34:15 +0530

    Added @refeshscope

--- product-app/src/main/java/com/swiftcart/product/config/ProductProperties.java
+++ product-app/src/main/java/com/swiftcart/product/config/ProductProperties.java
@@ -2,10 +2,12 @@ package com.swiftcart.product.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
+@RefreshScope
 public class ProductProperties {
   @Value("${product.property.name}")
   private String name;
