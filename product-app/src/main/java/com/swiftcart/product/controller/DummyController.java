e13b6eb5faa18747bef351e05668658f63b9c0a9
saphal4859 <saisaphal123@gmail.com>
2024-09-08 19:10:47 +0530

    Integrated logback and tested

--- product-app/src/main/java/com/swiftcart/product/controller/DummyController.java
+++ product-app/src/main/java/com/swiftcart/product/controller/DummyController.java
@@ -1,5 +1,7 @@
 package com.swiftcart.product.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DummyController {
 
+  // Create a logger instance
+  private static final Logger logger = LoggerFactory.getLogger(DummyController.class);
+
   @GetMapping("/hello")
   public String testEndPoint() {
+    // Log an info message
+    logger.info("Received request for /hello endpoint");
     return "Hello Swift cart";
   }
 
   @GetMapping("/health")
   public ResponseEntity<String> healthCheck() {
-
+    // Log an info message
+    logger.info("Received request for /health endpoint");
     return ResponseEntity.ok("Service is up and running!");
   }
 }
